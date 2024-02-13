1f73c170dfded95166af5933b3173a7a14ae71e4
Wilf Silver <git@wilfsilver.co.uk>
2024-02-13 11:07:20 +0000

    Only call the API once after signin

--- packages/frontend/src/components/page.tsx
+++ packages/frontend/src/components/page.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import type { APIClient } from "@esp-group-one/api-client";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import type { ReactNode } from "react";
+import { useRef, type ReactNode, useEffect } from "react";
 import { isNewUser, useAPIClient } from "../lib/auth.js";
 import { LoginButton } from "./auth.js";
 
@@ -24,25 +24,36 @@ interface WithTitleParams extends PageParamsBase {
 
 export function Page({ children, heading, setAPI }: PageParams) {
   const { isAuthenticated } = useAuth0();
+  const hasSetAPI = useRef<boolean>(false);
 
-  useAPIClient()
-    .then(async (client) => {
-      setAPI(client);
-      if (await isNewUser(client)) {
-        // TODO: Swap to redirect to signup form
-        client
-          .user()
-          .create({
-            description: "I am test bot 9000",
-            email: "test@bath.ac.uk",
-            name: "Test Bot",
-          })
-          .catch(console.error);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  // The idea here is that we only want to call the API once.
+  const api = useAPIClient();
+
+  // This seems to have a weird property of only running the function inside
+  // one at a time. Which means we don't have to deal with edge cases
+  useEffect(() => {
+    api
+      .then(async (client) => {
+        // This makes sure we haven't set the API before, as from testing Page
+        // is re-rendered twice in the same second.
+        if (!hasSetAPI.current) {
+          hasSetAPI.current = true;
+          setAPI(client);
+          if (await isNewUser(client)) {
+            // TODO: Swap to redirect to signup form
+            client
+              .user()
+              .create({
+                description: "I am test bot 9000",
+                email: "test@bath.ac.uk",
+                name: "Test Bot",
+              })
+              .catch(console.error);
+          }
+        }
+      })
+      .catch(console.error);
+  }, [api, isAuthenticated, setAPI]);
 
   return (
     <div className="w-full min-h-screen">
