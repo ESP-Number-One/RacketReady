6e6eee77d4f84c12c95e96a6604a0fd6f5c02c3b
larasheard <ls2548@bath.ac.uk>
2024-02-13 17:45:18 +0000

    please review

--- packages/frontend/src/components/form.tsx
+++ packages/frontend/src/components/form.tsx
@@ -1,27 +1,29 @@
-/* eslint-disable -- wejfhbew*/
-import { ReactNode, useCallback, useState } from "react";
+import type { ReactNode } from "react";
+import { useCallback, useState } from "react";
 
-interface Props {
+interface Propss {
   onSubmit: () => Promise<void>;
   children: ReactNode;
 }
 
-export function Form({ onSubmit, children }: Props) {
+export function Form({ onSubmit, children }: Propss) {
   const [error, setError] = useState("");
 
   const onSubmitWrapper = useCallback(() => {
-    onSubmit().catch((e) => setError(e));
+    onSubmit().catch((e: string) => {
+      setError(e.toString());
+    });
   }, [onSubmit]);
 
-  /* To do: switch to alternative buttons*/
+  // TODO: switch to alternative buttons
   return (
     <div className="padding-20px">
       {children}
       <div className="text-red">{error}</div>
       <button
-        className="backgroundColour-green borderRadius-3px padding-5px color-white paddingLeft-10px paddingRight-10px paddingTop-10px paddingBottom-8px"
-        type="submit"
+        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
         onClick={onSubmitWrapper}
+        type="submit"
       >
         Submit
       </button>
