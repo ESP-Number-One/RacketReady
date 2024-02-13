ee8bbe3e9408955ae5aede252d9dae7a61b8909d
Sammy99jsp <sammy99jsp@gmail.com>
2024-02-13 15:37:20 +0000

    🧹 Fix eslint for good (hopefully).

---
+++ packages/api-client/.eslintrc.cjs
@@ -0,0 +1,13 @@
+const { join } = require("node:path");
+
+/** @type {import("@types/eslint").ESLint.ConfigData} */
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+  },
+  parserOptions: {
+    project: join(__dirname, "tsconfig.json"),
+  },
+  extends: ["@esp-group-one"],
+};
