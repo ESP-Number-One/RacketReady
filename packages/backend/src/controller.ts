49a4d537ab3f1fc53ae5b04c2599e35a16bac634
Wilf Silver <git@wilfsilver.co.uk>
2024-02-14 09:50:03 +0000

    Add authentication to backend

--- packages/backend/src/controller.ts
+++ packages/backend/src/controller.ts
@@ -6,7 +6,9 @@ import {
   type QueryOptions,
 } from "@esp-group-one/types";
 import type { ObjectId, OptionalId } from "mongodb";
+import type { Request } from "express";
 import { Controller } from "tsoa";
+import { getUserId } from "./utils.js";
 
 /**
  * This class offers basically functions which implement the APIs for all
@@ -24,8 +26,7 @@ export class ControllerWrap<T, C> extends Controller {
   /**
    * Converts a creation type to the type without the id
    */
-  // eslint-disable-next-line @typescript-eslint/require-await -- This is a placeholder
-  async creationToObj(_: C): Promise<OptionalId<T>> {
+  creationToObj(_: C): OptionalId<T> {
     throw Error("Unimplemented");
   }
 
@@ -36,10 +37,15 @@ export class ControllerWrap<T, C> extends Controller {
     throw Error("Unimplemented");
   }
 
+  /**
+   * Safely gets an object and converts it to a response. This will not error
+   * out and so you can safely await the function
+   */
   protected async get(objId: ObjectId): Promise<WithError<T>> {
     return (await this.getCollection())
       .get(objId)
       .then((obj): WithError<T> => {
+        if (!obj) throw new Error("Id does not exist");
         return newAPISuccess(obj);
       })
       .catch((e) => {
@@ -49,6 +55,10 @@ export class ControllerWrap<T, C> extends Controller {
       });
   }
 
+  /**
+   * Safely finds the information for the current endpoints collection, if
+   * none is found it will just return an error
+   */
   protected async find(query: QueryOptions): Promise<WithError<T[]>> {
     return (await this.getCollection())
       .page(query)
@@ -64,9 +74,13 @@ export class ControllerWrap<T, C> extends Controller {
       });
   }
 
+  /**
+   * Safely creates a new object for the controller. However does not check if
+   * object exists already
+   */
   protected async create(requestBody: C): Promise<WithError<T>> {
     this.setStatus(201);
-    const obj = await this.creationToObj(requestBody);
+    const obj = this.creationToObj(requestBody);
 
     return (await this.getCollection())
       .insert(obj)
@@ -81,6 +95,34 @@ export class ControllerWrap<T, C> extends Controller {
         );
 
         return newAPIError("Failed to create new obj");
+      });
+  }
+
+  /**
+   * Gets the current user id and calls the callback if we are able to find
+   * one, if not we return an Error
+   * @param req - The request made to the endpoint to extract the user ID from
+   * @param callback - The function where you can use the userId knowing the
+   *   user does, in fact, exist which returns a response which we will pass
+   *   back to the returns
+   *
+   * @returns A response object which the callback has returned or an error if
+   *   we could not get the user id
+   */
+  protected async withUserId<R>(
+    req: Request,
+    callback: (user: ObjectId) => Promise<WithError<R>>,
+  ): Promise<WithError<R>> {
+    return getUserId(req)
+      .then((res) => {
+        if (res) return callback(res);
+        throw new Error("Could not get user");
+      })
+      .catch((e) => {
+        this.setStatus(500);
+        console.error(`Error when running with User: ${e}`);
+
+        return newAPIError("Unknown User");
       });
   }
 }
