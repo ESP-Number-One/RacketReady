e74b810026907380ea0d3fd0172e403ff194c2c3
larasheard <ls2548@bath.ac.uk>
2024-02-14 15:02:49 +0000

    conflicts resolved

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -1,30 +1,10 @@
-/* eslint-disable -- wejfhbew*/
 import { PageWithTitle } from "./components/page";
-import { Form } from "./components/form";
 
 function App() {
-  const handleSubmit = async () => {
-    console.log("Hello");
-  };
-
   return (
-    <div className="App">
-      <PageWithTitle currPage="home" heading="Testing">
-        <p>I am the child</p>
-        <Form onSubmit={handleSubmit}>
-          <label className="paddingBottom-3px">Enter your name:</label>
-          <br />
-          <input
-            className="boarder-2px-solid-black boarderRadius-4px paddingLeft-8px"
-            name="Name"
-            type="text"
-            boarder-black
-            value="Enter your name"
-            placeholder="Enter your username"
-          />
-        </Form>
-      </PageWithTitle>
-    </div>
+    <PageWithTitle currPage="home" heading="Testing">
+      <p>I am the child</p>
+    </PageWithTitle>
   );
 }
 
--- packages/frontend/src/components/form.tsx
+++ packages/frontend/src/components/form.tsx
@@ -19,9 +19,14 @@ export function Form({ onSubmit, children }: Propss) {
   return (
     <div className="padding-20px">
       {children}
-      <div className="text-red">{error}</div>
+      <div
+        className="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50"
+        role="alert"
+      >
+        <span className="font-medium">{error}</span>
+      </div>
       <button
-        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
         onClick={onSubmitWrapper}
         type="submit"
       >
