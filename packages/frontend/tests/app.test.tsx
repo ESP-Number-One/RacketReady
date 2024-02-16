d7eacff287be41bbc76b7c9498ec35f218a15ba2
Sammy99jsp <sammy99jsp@gmail.com>
2024-02-16 12:43:21 +0000

    Add vscode compat. for `frontend` tests + Example React Test

--- packages/frontend/tests/app.test.tsx
+++ packages/frontend/tests/app.test.tsx
@@ -1,3 +1,15 @@
-test("Apples", () => {
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+test("Test message", () => {
   expect(3).toBe(3);
+  const testMessage = "Test Message";
+  render(<>{testMessage}</>);
+
+  // query* functions will return the element or null if it cannot be found
+  // get* functions will return the element or throw an error if it cannot be found
+
+  const div = screen.queryByText(testMessage);
+  expect(div).toBeInTheDocument();
+  expect(div).toHaveTextContent(testMessage);
 });
