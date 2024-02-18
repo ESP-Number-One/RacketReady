94363e777d4fd9c612dfdd93d985f51aa443ace1
CarolineMorton12 <cm2781@bath.ac.uk>
2024-02-18 17:04:47 +0000

    fixed errors

--- packages/frontend/src/components/Stars.tsx
+++ packages/frontend/src/components/Stars.tsx
@@ -1,44 +1,44 @@
-import Star from "../star.svg";
 import { faStar as faSolidStar } from "@fortawesome/free-solid-svg-icons";
 import { faStar as faRegStar } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function StarButton(props: {
   onClick: (rating: number) => void;
   currentRating: number;
   rating: number;
+  disabled: boolean;
 }) {
   return (
-    <button onClick={() => props.onClick(props.rating)}>
+    <button
+      disabled={props.disabled}
+      onClick={() => {
+        props.onClick(props.rating);
+      }}
+      type="button"
+    >
       {props.currentRating >= props.rating ? (
         <FontAwesomeIcon icon={faSolidStar} size="2x" />
       ) : (
         <FontAwesomeIcon icon={faRegStar} size="2x" />
       )}
-      {/*<img*/}
-      {/*  src={Star}*/}
-      {/*  className="w-10 h-10 border-2 border-squash-yellow"*/}
-      {/*  style={{*/}
-      {/*    backgroundColor:*/}
-      {/*      props.currentRating >= props.rating ? "yellow" : undefined,*/}
-      {/*  }}*/}
-      {/*  alt={"star"}*/}
-      {/*/>*/}
     </button>
   );
 }
 
-export default function Stars(props: {
+export function Stars(props: {
   rating: number;
   onRatingChange: (newRating: number) => void;
+  disabled: boolean;
 }) {
   return (
     <div className="space-x-2">
       {[1, 2, 3, 4, 5].map((e) => (
         <StarButton
-          onClick={props.onRatingChange}
           currentRating={props.rating}
-          rating={e}
           key={e}
+          onClick={props.onRatingChange}
+          rating={e}
+          disabled={props.disabled}
         />
       ))}
     </div>
