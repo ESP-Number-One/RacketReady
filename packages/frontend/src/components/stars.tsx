d4ec2a47e081f5e38524b0f9a7af945d321fe463
Caroline Morton <cm2781@bath.ac.uk>
2024-02-18 18:37:19 +0000

    Add the stars feature (#23)
    * added the stars feature
    
    * fixed errors
    
    * Fix linting
    
    ---------
    
    Co-authored-by: Wilf Silver <git@wilfsilver.co.uk>

---
+++ packages/frontend/src/components/stars.tsx
@@ -0,0 +1,46 @@
+import { faStar as faSolidStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faRegStar } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function StarButton(props: {
+  onClick: (rating: number) => void;
+  currentRating: number;
+  rating: number;
+  disabled: boolean;
+}) {
+  return (
+    <button
+      disabled={props.disabled}
+      onClick={() => {
+        props.onClick(props.rating);
+      }}
+      type="button"
+    >
+      {props.currentRating >= props.rating ? (
+        <FontAwesomeIcon icon={faSolidStar} size="2x" />
+      ) : (
+        <FontAwesomeIcon icon={faRegStar} size="2x" />
+      )}
+    </button>
+  );
+}
+
+export function Stars(props: {
+  rating: number;
+  onRatingChange: (newRating: number) => void;
+  disabled: boolean;
+}) {
+  return (
+    <div className="space-x-2">
+      {[1, 2, 3, 4, 5].map((e) => (
+        <StarButton
+          currentRating={props.rating}
+          disabled={props.disabled}
+          key={e}
+          onClick={props.onRatingChange}
+          rating={e}
+        />
+      ))}
+    </div>
+  );
+}
