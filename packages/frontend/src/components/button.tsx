93ab421917adcc04112e5edf9569870bcddfc2a6
Thomas Canning <thomas500book@gmail.com>
2024-02-19 17:18:22 +0000

    Finishing Buttons

--- packages/frontend/src/components/button.tsx
+++ packages/frontend/src/components/button.tsx
@@ -11,20 +11,14 @@ export function Button({
   children,
   icon,
 }: ButtonProps) {
-  const hoverColor = backgroundColor.includes("green")
-    ? "hover:bg-p-green-200"
-    : backgroundColor.includes("red")
-      ? "hover:bg-p-red-200"
-      : "";
-
   return (
     <button
       type="button"
-      className={`font-body text-2xl font-black text-white ${backgroundColor} ${hoverColor} active:scale-95 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 ${
+      className={`font-body text-3xl font-extrabold text-white ${backgroundColor} active:scale-95 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 ${
         icon ? "justify-start" : "justify-center"
       } inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 w-full transform transition duration-150 ease-in-out`}
     >
-      {icon && <span className="mr-2 align-middle">{icon}</span>}
+      {icon && <span className="mr-4 align-middle">{icon}</span>}
       {children}
     </button>
   );
