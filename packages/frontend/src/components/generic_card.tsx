59153218e63ceb3e91bb593b636f07ebc374058d
Ruaidhri O'Shea <124783806+ros40@users.noreply.github.com>
2024-02-22 19:20:31 +0000

    Rewritten the search and card list components, yet to test with the latest backend or with any card components

--- packages/frontend/src/components/generic_card.tsx
+++
@@ -1,8 +0,0 @@
-interface genericCardProps<T> {
-  cardSubject: T;
-}
-
-export function GenericCard<T>({ cardSubject: T }: genericCardProps<T>) {
-  // if T has a name field, make this the title
-  return <div className="fill-pg-bar-gray rounded-md"></div>;
-}
--- packages/frontend/src/components/search_competitors.tsx
+++ packages/frontend/src/components/search_competitors.tsx
@@ -1,67 +1,86 @@
-import {
-  faArrowRight,
-  faMagnifyingGlass,
-} from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { memo, useState } from "react";
-import { GenericCardList } from "./generic_card_list";
+import { memo } from "react";
+import { CardList } from "./cards/card_list";
 
-interface searchProps {
-  onSubmit: (query: string) => void;
+type SearchableObject = "user" | "league" | "match";
+
+interface SearchProps {
+  searchBtnPressedWrapper: (obj: SearchableObject) => void;
+  searchType: SearchableObject;
 }
 
-export const SearchCompetitor = memo(function SearchCompetitor({
-  onSubmit,
-}: searchProps) {
-  const [hideSearch, setHideSearch] = useState(true);
-  const [searchQuery, setSearchQuery] = useState("");
-
-  function onSubmitWrapper() {
-    onSubmit(searchQuery);
+export const searchBtnPressed = (obj: SearchableObject) => {
+  let result = (
+    <CardList
+      clientType="user"
+      queryOptions={{ query: {}, sort: "", pageStart: 0, pageSize: 0 }}
+    />
+  );
+  switch (obj) {
+    case "user": {
+      // get page 0 of users and convert to cardDetail
+      result = (
+        <CardList
+          clientType="user"
+          queryOptions={{
+            query: { name: "" },
+            sort: "",
+            pageStart: 0,
+            pageSize: 20,
+          }}
+        />
+      );
+      break;
+    }
+    case "league": {
+      // get page 0 of leagues and convert to cardDetail
+      result = (
+        <CardList
+          clientType="league"
+          queryOptions={{
+            query: { name: "" },
+            sort: "",
+            pageStart: 0,
+            pageSize: 20,
+          }}
+        />
+      );
+      break;
+    }
+    case "match": {
+      // get page 0 of matches and convert to cardDetail
+      result = (
+        <CardList
+          clientType="match"
+          queryOptions={{
+            query: { owner: "" },
+            sort: "",
+            pageStart: 0,
+            pageSize: 20,
+          }}
+        />
+      );
+      break;
+    }
   }
+  return result;
+};
+
+export const SearchButton = memo(function SearchButton({
+  searchBtnPressedWrapper,
+  searchType,
+}: SearchProps) {
   return (
     <div className="display-flex">
       <button
         onClick={() => {
-          setHideSearch(!hideSearch);
+          searchBtnPressedWrapper(searchType);
         }}
         type="button"
       >
         <FontAwesomeIcon icon={faMagnifyingGlass} />
       </button>
-      <div hidden={hideSearch}>
-        <input
-          inputMode="text"
-          name="search"
-          onChange={(i) => {
-            setSearchQuery(i.target.value);
-          }}
-          onKeyUp={(event) => {
-            if (event.key === "Enter") {
-              onSubmitWrapper();
-            }
-          }}
-          onSubmit={() => {
-            onSubmitWrapper();
-          }}
-          placeholder="Search for competitors..."
-          required
-          type="text"
-        />
-        <button
-          onClick={() => {
-            onSubmitWrapper();
-          }}
-          onSubmit={() => {
-            onSubmitWrapper();
-          }}
-          type="submit"
-          value="Search"
-        >
-          <FontAwesomeIcon icon={faArrowRight} />
-        </button>
-      </div>
-      <GenericCardList cardSubjects={[]}></GenericCardList>
     </div>
   );
 });
