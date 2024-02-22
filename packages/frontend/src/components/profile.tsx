4b97ad18d25a023e7abf3f8d6357b21cc1678705
castellacodes <carissa2206@gmail.com>
2024-02-22 19:43:39 +0000

    changed dimensions to w-24 and h-24

--- packages/frontend/src/components/profile.tsx
+++ packages/frontend/src/components/profile.tsx
@@ -4,7 +4,7 @@ interface ProfPic {
 
 export function Profile({ imgSrc }: ProfPic) {
   return (
-    <div className={`w-[102px] h-[107px] rounded-2xl overflow-hidden`}>
+    <div className={`w-24 h-24 rounded-2xl overflow-hidden`}>
       <img src={imgSrc} className={`w-full h-full object-cover`} />
     </div>
   );
