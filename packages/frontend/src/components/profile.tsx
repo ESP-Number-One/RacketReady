f4ac986813f7e57a5f98bca52db28436ff29fee3
castellacodes <101331945+castellacodes@users.noreply.github.com>
2024-02-23 17:17:56 +0000

    Add profile photo component that takes URL (#47)
    * Profile photo component that takes URL
    
    * change dimensions to w-24 and h-24

---
+++ packages/frontend/src/components/profile.tsx
@@ -0,0 +1,11 @@
+interface ProfPic {
+  imgSrc: string;
+}
+
+export function Profile({ imgSrc }: ProfPic) {
+  return (
+    <div className={`w-24 h-24 rounded-2xl overflow-hidden`}>
+      <img src={imgSrc} className={`w-full h-full object-cover`} />
+    </div>
+  );
+}
