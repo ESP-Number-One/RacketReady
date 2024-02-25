9bcda89425f12889a28a5855c1dc451202a1c3dd
larasheard <ls2548@bath.ac.uk>
2024-02-25 18:32:24 +0000

    more future proof

--- packages/frontend/src/components/select_sports.tsx
+++ packages/frontend/src/components/select_sports.tsx
@@ -1,4 +1,4 @@
-import { Sport } from "@esp-group-one/types";
+import type { Sport } from "@esp-group-one/types";
 import { useState } from "react";
 import type { ChangeEvent } from "react";
 
@@ -10,42 +10,34 @@ export function SelectSport({
   sports: sportsOptions,
   currentSport,
 }: SportListProps & { currentSport: Sport }) {
-  const [sport, setSport] = useState(currentSport);
-  const [colour, setColour] = useState(currentSport);
-
-  const options = [
-    { colour: "tennis", label: "Tennis" },
-    { colour: "badminton", label: "Badminton" },
-    { colour: "squash", label: "Squash" },
-  ];
+  const [sport, setSport] = useState(currentSport.toString());
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    switch (e.target.value) {
-      case "tennis":
-    }
     setSport(e.target.value);
-    setColour(getColour());
   };
 
   return (
     <div>
       <select
-        className={`text-white bg-${colour} hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-body rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center w-full`}
+        className={`text-white ${getColour(
+          sport,
+        )} hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-body rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center w-full`}
         value={sport}
         id="dropdown"
         onChange={(e) => {
           handleChange(e);
         }}
       >
-        {options.map((option, index) => (
+        {sportsOptions.map((option, index) => (
           <option
             className={`text-white ${getColour(
-              option.colour as Sport,
-            )} hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-body rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">Dropdown button <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6`}
+              option.toString(),
+            )} hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-body rounded-lg text-xl px-5 py-2.5 text-center inline-flex items-center`}
             key={index}
-            value={option.label}
+            value={option.toString()}
           >
-            {option.label}
+            {option.toString().charAt(0).toUpperCase() +
+              option.toString().slice(1)}
           </option>
         ))}
       </select>
@@ -53,13 +45,6 @@ export function SelectSport({
   );
 }
 
-function getColour(sport: Sport) {
-  switch (sport) {
-    case Sport.Badminton:
-      return "bg-badminton";
-    case Sport.Squash:
-      return "bg-squash";
-    case Sport.Tennis:
-      return "bg-tennis";
-  }
+function getColour(sport: string) {
+  return `bg-${sport}`;
 }
