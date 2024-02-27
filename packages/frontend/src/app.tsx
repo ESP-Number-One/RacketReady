acf02f167220719231dd4a0582e3c3aa22b6bb1c
castellacodes <carissa2206@gmail.com>
2024-02-27 17:21:28 +0000

    Changed matchcard to use Moment.js

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -1,3 +1,6 @@
+import { Sport } from "@esp-group-one/types";
+import moment from "moment";
+import { MatchCard } from "./components/matchcard.js";
 import { PageWithTitle } from "./components/page.js";
 
 export function App() {
@@ -8,7 +11,14 @@ export function App() {
       heading="Testing"
       setAPI={console.log}
     >
-      <p>I am the child</p>
+      <MatchCard
+        sportsTag={Sport.Squash}
+        profilePic="https://trello.com/1/cards/65d51a05435c78cb5cb4eacf/attachments/65d51a18f3adf7a593ce5788/download/image.png"
+        name="Sammy N"
+        startTime={moment("2024-02-22 15:15:00")} //assuming times passed from backend is formatted correctly
+        endTime={moment("2024-02-22 17:15:00")}
+        link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      />
     </PageWithTitle>
   );
 }
