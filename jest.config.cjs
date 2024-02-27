2b6b330442b868ed8cd2106a68436413e979cddf
Wilf Silver <git@wilfsilver.co.uk>
2024-02-27 15:09:14 +0000

    Make frontend jest config alphabetical order

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,19 +5,24 @@ const config = {
   coverageReporters: ["html"],
   projects: [
     {
-      rootDir: "packages/frontend",
-      displayName: "Frontend",
       collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
+      coveragePathIgnorePatterns: ["<rootDir>/tests/utils.tsx"],
+      displayName: "Frontend",
+      moduleFileExtensions: ["ts", "tsx", "js"],
       preset: "ts-jest",
+      rootDir: "packages/frontend",
       testEnvironment: "jsdom",
       transform: {
-        ".(ts|tsx)": "ts-jest",
+        "\\.(ts|tsx)$": "ts-jest",
       },
       testRegex: "\\.(test|spec)\\.(ts|tsx)$",
       moduleFileExtensions: ["ts", "tsx", "js"],
       coveragePathIgnorePatterns: ["<rootDir>/tests/utils.tsx"],
     },
   ],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
   testRegex: "\\.(test|spec)\\.(ts|tsx)$",
   moduleFileExtensions: ["ts", "tsx", "js"],
 };
