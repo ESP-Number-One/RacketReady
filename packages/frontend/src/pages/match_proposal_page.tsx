e5e0c50d28ee8414fbaaef868504549c38171727
Ruaidhri O'Shea <124783806+ros40@users.noreply.github.com>
2024-03-07 10:05:05 +0000

    Implement conversion for match proposals to use in the page

---
+++ packages/frontend/src/pages/match_proposal_page.tsx
@@ -0,0 +1,57 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import type { ReactNode } from "react";
+import { useContext } from "react";
+import { Page } from "../components/page";
+import { Header } from "../components/page/header";
+import { CardList } from "../components/card_list";
+import { API } from "../state/auth";
+import { Cards } from "../utils/types_to_cards";
+import { useViewNav } from "../state/nav";
+
+export function MatchProposal() {
+  const api = useContext(API);
+
+  const viewNavigate = useViewNav();
+
+  const nextPage = (pageNum: number): Promise<ReactNode[]> => {
+    let result: ReactNode[];
+    api
+      .match()
+      .findProposed({ pageStart: pageNum })
+      .then((matches) => {
+        result = Cards.fromMatches(matches, api);
+      })
+      .catch((error) => {
+        console.warn(error);
+        result = [];
+      });
+    return new Promise<ReactNode[]>((res) => {
+      res(result);
+    });
+  };
+
+  return (
+    <Page>
+      <Page.Header>
+        <Header.Back />
+        Page Header
+        <Header.Right>
+          <FontAwesomeIcon icon={faPlus} />
+        </Header.Right>
+      </Page.Header>
+      <Page.Body>
+        <CardList nextPage={nextPage} />
+      </Page.Body>
+      <Page.Footer>
+        <button
+          onClick={() => {
+            viewNavigate("/match-proposal");
+          }}
+        >
+          MatchProposal
+        </button>
+      </Page.Footer>
+    </Page>
+  );
+}
--- packages/frontend/src/utils/types_to_cards.tsx
+++ packages/frontend/src/utils/types_to_cards.tsx
@@ -1,23 +1,55 @@
-import type { CensoredUser, League, Match, User } from "@esp-group-one/types";
+import {
+  MatchStatus,
+  type CensoredUser,
+  type League,
+  type Match,
+  type User,
+} from "@esp-group-one/types";
+import type { ReactNode } from "react";
+import type { APIClient } from "@esp-group-one/api-client";
+import { Proposal } from "../components/proposal";
 
 export namespace Cards {
   export function fromUsers(users: User[] | CensoredUser[]) {
     console.error("Not implemented");
-    return users.map((user, _idx, _arr) => {
-      return <div key={user._id.toString()} />;
+    return users.map((user) => {
+      return (<div key={user._id.toString()} />) as ReactNode;
     });
   }
 
   export function fromLeagues(leagues: League[]) {
     console.error("Not implemented");
-    return leagues.map((league, _idx, _arr) => {
-      return <div key={league._id.toString()} />;
+    return leagues.map((league) => {
+      return (<div key={league._id.toString()} />) as ReactNode;
     });
   }
-  export function fromMatches(matches: Match[]) {
-    console.error("Not implemented");
-    return matches.map((match, _idx, _arr) => {
-      return <div key={match._id.toString()} />;
+  export function fromMatches(matches: Match[], api: APIClient) {
+    return matches.map((match) => {
+      let card: ReactNode;
+      switch (match.status) {
+        case MatchStatus.Accepted: {
+          console.warn("Unimplemented conversion!");
+          card = <div className="p-5 bg-p-grey-100">Unimplemented</div>;
+          break;
+        }
+        case MatchStatus.Complete: {
+          console.warn("Unimplemented conversion!");
+          card = <div className="p-5 bg-p-grey-100">Unimplemented</div>;
+          break;
+        }
+        case MatchStatus.Request: {
+          card = (
+            <Proposal
+              data={match}
+              onAccept={() => {
+                api.match().accept(match._id).then().catch(console.warn);
+              }}
+            />
+          ) as ReactNode;
+          break;
+        }
+      }
+      return card;
     });
   }
 }
