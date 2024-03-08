35e8bb7b3a1daaf2068fa002dbae7818ed574732
Caroline Morton <cm2781@bath.ac.uk>
2024-03-08 17:14:46 +0000

    Add League Versus Match card (#58)
    * added a lovely matchcard
    
    * changed file name
    
    * Unit tests for VersusMatchCard. Found fundamental error in the component and fixed it hehehe (testing is amazing)
    
    * Clean up
    
    ---------
    
    Co-authored-by: Wilf Silver <git@wilfsilver.co.uk>

---
+++ packages/frontend/src/components/card/versus_match.tsx
@@ -0,0 +1,40 @@
+/**
+ * Once given a score in the form:
+ *
+ * ```js
+ * const score = {
+ *   "<person_name_1>": '<person_1_score>',
+ *   "<person_name_2>": '<person_2_score>',
+ * };
+ * ```
+ */
+export function VersusMatchCard(props: { score: Record<string, number> }) {
+  const [firstKey, secondKey] = Object.keys(props.score);
+  const firstValue = props.score[firstKey];
+  const secondValue = props.score[secondKey];
+  return (
+    <div className="bg-gray-400 text-center w-min rounded-lg text-white overflow-hidden">
+      <div
+        className={`${
+          secondValue <= firstValue ? "bg-p-green-200" : ""
+        } flex justify-between whitespace-nowrap text-xl font-bold items-center`}
+      >
+        <p className="m-2">{firstKey}</p>
+        <p className="ml-2 p-2">{firstValue}</p>
+      </div>
+      <div className="grid grid-cols-3 items-center">
+        <div className="bg-white h-1" />
+        <p className="leading-[0]">vs</p>
+        <div className="bg-white h-1" />
+      </div>
+      <div
+        className={`${
+          firstValue <= secondValue ? "bg-p-green-200" : ""
+        } flex justify-between whitespace-nowrap text-xl font-bold items-center`}
+      >
+        <p className="m-2">{secondKey}</p>
+        <p className="ml-2 p-2">{secondValue}</p>
+      </div>
+    </div>
+  );
+}
---
+++ packages/frontend/tests/components/card/versus_match.test.tsx
@@ -0,0 +1,104 @@
+import { render } from "@testing-library/react";
+import { VersusMatchCard } from "../../../src/components/card/versus_match";
+
+/* Jest + React Guides: https://jestjs.io/docs/tutorial-react */
+
+// Group of related tests.
+describe("VersusMatchCard", () => {
+  //     \/ Describe individual test here.
+  test("Correct scores displayed", () => {
+    const score = {
+      Lara: 10,
+      Hugo: 1,
+    };
+
+    const component = render(<VersusMatchCard score={score} />);
+    expect(component.container).toBeInTheDocument();
+
+    const matchCard = component.container.children.item(0) as HTMLElement;
+    expect(matchCard).not.toBeNull();
+
+    const div1 = matchCard.children.item(0) as HTMLElement;
+    const div2 = matchCard.children.item(2) as HTMLElement;
+
+    expect(div1).not.toBeNull();
+    expect(div2).not.toBeNull();
+
+    const p11 = div1.children.item(0);
+    expect(p11).not.toBeNull();
+    expect(p11?.textContent).toBe("Lara");
+    const p12 = div1.children.item(1);
+    expect(p12).not.toBeNull();
+    expect(p12?.textContent).toBe("10");
+
+    const p21 = div2.children.item(0);
+    expect(p21).not.toBeNull();
+    expect(p21?.textContent).toBe("Hugo");
+    const p22 = div2.children.item(1);
+    expect(p22).not.toBeNull();
+    expect(p22?.textContent).toBe("1");
+  });
+
+  test("Correct winner (first)", () => {
+    const score = {
+      Lara: 10,
+      Hugo: 1,
+    };
+
+    const component = render(<VersusMatchCard score={score} />);
+    expect(component.container).toBeInTheDocument();
+
+    const matchCard = component.container.children.item(0) as HTMLElement;
+    expect(matchCard).not.toBeNull();
+
+    const div1 = matchCard.children.item(0) as HTMLElement;
+    const div2 = matchCard.children.item(2) as HTMLElement;
+    expect(div1).not.toBeNull();
+    expect(div2).not.toBeNull();
+
+    expect(div1.classList.contains("bg-p-green-200")).toBe(true);
+    expect(div2.classList.contains("bg-p-green-200")).toBe(false);
+  });
+
+  test("Correct winner (second)", () => {
+    const score = {
+      Lara: 1,
+      Hugo: 10,
+    };
+
+    const component = render(<VersusMatchCard score={score} />);
+    expect(component.container).toBeInTheDocument();
+
+    const matchCard = component.container.children.item(0) as HTMLElement;
+    expect(matchCard).not.toBeNull();
+
+    const div1 = matchCard.children.item(0) as HTMLElement;
+    const div2 = matchCard.children.item(2) as HTMLElement;
+    expect(div1).not.toBeNull();
+    expect(div2).not.toBeNull();
+
+    expect(div1.classList.contains("bg-p-green-200")).toBe(false);
+    expect(div2.classList.contains("bg-p-green-200")).toBe(true);
+  });
+
+  test("Correct handling of equal scores", () => {
+    const score = {
+      Hugo1: 1,
+      Hugo2: 1,
+    };
+
+    const component = render(<VersusMatchCard score={score} />);
+    expect(component.container).toBeInTheDocument();
+
+    const matchCard = component.container.children.item(0) as HTMLElement;
+    expect(matchCard).not.toBeNull();
+
+    const div1 = matchCard.children.item(0) as HTMLElement;
+    const div2 = matchCard.children.item(2) as HTMLElement;
+    expect(div1).not.toBeNull();
+    expect(div2).not.toBeNull();
+
+    expect(div1.classList.contains("bg-p-green-200")).toBe(true);
+    expect(div2.classList.contains("bg-p-green-200")).toBe(true);
+  });
+});
