536ab9f9b7db776f679bb38d99d9fda8bcc7e4c3
Ruaidhri O'Shea <124783806+ros40@users.noreply.github.com>
2024-03-10 09:40:42 +0000

    Blank discover league page

---
+++ packages/frontend/src/pages/discover_leagues.tsx
@@ -0,0 +1,13 @@
+import { Page } from "../components/page";
+import { Header } from "../components/page/header";
+
+export function DiscoverLeagues() {
+  return (
+    <Page>
+      <Page.Header>
+        <Header.Back />
+        Suggested Leagues
+      </Page.Header>
+    </Page>
+  );
+}
