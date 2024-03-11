bf89fb234d887886c0cb935c6111919bc37a8ae3
Ruaidhri O'Shea <124783806+ros40@users.noreply.github.com>
2024-03-11 12:23:57 +0000

    Add props for extra user info not found in censored users

--- packages/frontend/src/components/rec_profile.tsx
+++ packages/frontend/src/components/rec_profile.tsx
@@ -1,18 +1,20 @@
-import { calculateAverageRating, type User } from "@esp-group-one/types";
+import { calculateAverageRating } from "@esp-group-one/types";
+import type { CensoredUser } from "@esp-group-one/types";
 import moment from "moment";
 import { ProfilePic } from "./profile_pic";
 import { Stars } from "./stars";
 
 interface Info {
-  user: User;
+  user: CensoredUser;
+  pfp: string;
+  availability: string[];
   displayAbility: boolean;
 }
 
-export function RecProfile({ user, displayAbility }: Info) {
+export function RecProfile({ user, pfp, availability, displayAbility }: Info) {
   const sports = user.sports;
   const name = user.name;
   const desc = user.description;
-  const pfp = user.profilePicture;
   const rating = calculateAverageRating(user.rating);
 
   return (
@@ -31,27 +33,33 @@ export function RecProfile({ user, displayAbility }: Info) {
         <div className="flex flex-row-reverse p-2">
           <Stars rating={rating} />
         </div>
-        <div className="max-w-full w-full text-white text-center max-h-fit overflow-clip font-body pb-3 px-5">
+        <div
+          className={
+            availability.length > 0
+              ? "max-w-full w-full text-white text-center max-h-fit overflow-clip font-body pb-3 px-5"
+              : "max-w-full w-full text-white text-center max-h-fit overflow-clip font-body pb-3 px-5 rounded-b-lg"
+          }
+        >
           <p className="line-clamp-3">{desc}</p>
         </div>
       </div>
-      <div className="bg-slate-400 px-5 pt-2 pb-2 mt-1 space-y-2 overflow-scroll rounded-b-lg">
-        {user.availability.map((time, i) => {
-          return (
-            <div
-              className="bg-slate-600 flex justify-between text-white text-xl text-bold text-body px-5 pt-3 pb-3 rounded-lg"
-              key={`${user._id.toString()}-${i}`}
-            >
-              <p className="text-left">{`${moment(time.timeStart).format(
-                "hh:mm",
-              )}-${moment(time.timeEnd).format("hh:mm")}`}</p>
-              <p className="text-right">
-                {moment(time.timeStart).format("dddd")}
-              </p>
-            </div>
-          );
-        })}
-      </div>
+      {availability.length > 0 ? (
+        <div className="bg-slate-400 px-5 pt-2 pb-2 mt-1 space-y-2 overflow-scroll rounded-b-lg">
+          {availability.map((time, i) => {
+            return (
+              <div
+                className="bg-slate-600 flex justify-between text-white text-xl text-bold text-body px-5 pt-3 pb-3 rounded-lg"
+                key={`${user._id.toString()}-${i}`}
+              >
+                <p className="text-left">{`${moment(time).format(
+                  "hh:mm",
+                )}-${moment(time).add(1, "hours").format("hh:mm")}`}</p>
+                <p className="text-right">{moment(time).format("dddd")}</p>
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
     </div>
   );
 }
