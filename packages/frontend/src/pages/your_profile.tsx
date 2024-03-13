bd9ff7d36b3d1c5057ea48a6dbe359a7950aecc0
CarolineMorton12 <cm2781@bath.ac.uk>
2024-03-13 16:12:17 +0000

    Made profile page

--- packages/frontend/src/pages/your_profile.tsx
+++ packages/frontend/src/pages/your_profile.tsx
@@ -3,27 +3,68 @@ import { Stars } from "../components/stars";
 import { useViewNav } from "../state/nav";
 import { Page } from "../components/page";
 import { useState } from "react";
-import { Profile } from "../components/profile.tsx";
-import { RadioButton } from "../components/radio_button.tsx";
+import { ProfilePic } from "../components/profile_pic.tsx";
+import { AbilityLevel, Sport } from "@esp-group-one/types";
+import { PICTURES } from "@esp-group-one/types/build/tests/helpers/utils";
+import { Button } from "../components/button.tsx";
+import {
+  faCalendar,
+  faGear,
+  faPenToSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from "react-router-dom";
 
 export function YourProfile() {
   const [rating, setRating] = useState(4);
   const viewNavigate = useViewNav();
 
+  const navigate = useNavigate();
+
   return (
     <Page>
+      <Page.Header>
+        <ProfilePic
+          image={`data:image/webp;base64,${PICTURES[0]}`}
+          sports={[{ sport: Sport.Tennis, ability: AbilityLevel.Beginner }]}
+        />
+      </Page.Header>
       <Page.Body>
-        <Profile imgSrc={"data:image/png;base64"} />
-        <p>background picture (profile picture component)</p>
-        <p>sports</p>
-        <p>Level</p>
-        <p>Name</p>
-        <Stars rating={rating} onRatingChange={setRating} />
-        <p>Description</p>
-        <RadioButton name={"Hugo"} value={"Tennis"} />
-        <p>Calendar (buttons component)</p>
-        <p>Edit Profile</p>
-        <p>Settings</p>
+        <p className={"text-right font-title py-2 px-3 text-3xl font-bold"}>
+          Name
+        </p>
+        <div className={"flex justify-end "}>
+          <Stars
+            rating={rating}
+            onRatingChange={setRating}
+            disabled={true}
+            size={"lg"}
+          />
+        </div>
+        <p className={"py-2 px-3 text-center"}>Description</p>
+        <div className={"flex flex-col space-y-4"}>
+          <Button
+            backgroundColor={"bg-p-grey-100"}
+            onClick={() => navigate("/calendar")}
+          >
+            <FontAwesomeIcon className={"mr-2"} icon={faCalendar} />
+            Calendar
+          </Button>
+          <Button
+            backgroundColor={"bg-p-grey-100"}
+            onClick={() => navigate("/editProfile")}
+          >
+            <FontAwesomeIcon className={"mr-2"} icon={faPenToSquare} />
+            Edit Profile
+          </Button>
+          <Button
+            backgroundColor={"bg-p-grey-100"}
+            onClick={() => navigate("/settings")}
+          >
+            <FontAwesomeIcon className={"mr-2"} icon={faGear} />
+            Settings
+          </Button>
+        </div>
       </Page.Body>
       <Page.Footer>
         <button
