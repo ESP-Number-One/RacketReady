f72856a48221c1ef8e76d94f4261a26be17f5ff0
Sammy99jsp <sammy99jsp@gmail.com>
2024-03-22 03:10:52 +0000

    Add testing for `league/your`

---
+++ packages/frontend/tests/pages/league/your.test.tsx
@@ -0,0 +1,45 @@
+import { act, render } from "@testing-library/react";
+// eslint-disable-next-line import/no-named-as-default -- WHYYY
+import userEvent from "@testing-library/user-event";
+import {
+  getLeague,
+  getMatch,
+} from "@esp-group-one/types/build/tests/helpers/utils";
+import type { ObjectId } from "@esp-group-one/types";
+import { MockAPI, wait } from "../../helpers/utils";
+import { PageTester } from "../helpers";
+import { YourLeagues } from "../../../src/pages/league/your";
+import { mockRouting } from "../../__meta__";
+
+test("Render", async () => {
+  const { useNavigate } = await mockRouting();
+  const navFn = jest.fn();
+  useNavigate.mockReturnValueOnce(navFn);
+
+  const MockedAPI = MockAPI({
+    league: () => ({
+      find: () => Promise.resolve([getLeague({})]),
+    }),
+    match: () => ({
+      find: ({ query: { league } = { league: undefined } }) =>
+        Promise.resolve([getMatch({ league: league as ObjectId })]),
+    }),
+  });
+
+  const comp = render(
+    <PageTester route="/leagues">
+      <MockedAPI>
+        <YourLeagues />
+      </MockedAPI>
+    </PageTester>,
+  );
+
+  await act(() => wait(100));
+  await userEvent.click(
+    comp.getByText("Discover more").parentElement as unknown as HTMLElement,
+  );
+
+  act(() => void 0);
+
+  expect(navFn).toHaveBeenCalledWith("/leagues/discover", expect.anything());
+});
