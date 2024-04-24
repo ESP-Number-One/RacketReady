4ded0ab79c3bb505334c99b2bf75e7517aa24f51
Wilf Silver <git@wilfsilver.co.uk>
2024-04-24 12:53:33 +0100

    Add new form for availability

--- packages/frontend/src/pages/me/availability.tsx
+++ packages/frontend/src/pages/me/availability.tsx
@@ -9,9 +9,10 @@ import { Header } from "../../components/page/header";
 import { RadioButton } from "../../components/form/radio_button";
 import { Input } from "../../components/form/input";
 import { Feed } from "../../components/card/feed";
+import Select from "react-select";
 
 export interface AvailabilityCreator {
-  date: string;
+  date: string[];
   start: string;
   end: string;
   recurring: number | undefined;
@@ -19,6 +20,201 @@ export interface AvailabilityCreator {
 }
 
 type RecurringAvailability = Availability & { recurring: Duration };
+
+export function NewForm({
+  info,
+  setInfo,
+}: {
+  info: AvailabilityCreator;
+  setInfo: (a: AvailabilityCreator) => void;
+}) {
+  const now = moment();
+
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  const options = days.map((label, i) => {
+    let date = moment().day(i + 1);
+    if (date.isBefore(now)) {
+      date = date.add(1, "week");
+    }
+    return {
+      value: date.toISOString(),
+      label,
+    };
+  });
+
+  return (
+    <div className="flex-none">
+      <Select
+        isMulti
+        options={options}
+        className="w-full mt-2"
+        onChange={(selected) => {
+          setInfo({
+            ...info,
+            date: selected.map((s) => s.value),
+            recurring: 1,
+            recurringUnit: "weeks",
+          });
+        }}
+        required
+      />
+
+      <div className="grid grid-cols-2 gap-2 mt-2">
+        <div>
+          <label className="font-body font-bold text-xl" htmlFor="start">
+            Start
+          </label>
+          <Input
+            id="start"
+            type="time"
+            onChange={(start) => {
+              setInfo({ ...info, start });
+            }}
+            value={info.start}
+            required
+          />
+        </div>
+        <div>
+          <div className="flex justify-end">
+            <label className="font-body font-bold text-xl" htmlFor="end">
+              End
+            </label>
+          </div>
+          <Input
+            value={info.end}
+            id="end"
+            type="time"
+            onChange={(end) => {
+              setInfo({ ...info, end });
+            }}
+            required
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function OldForm({
+  info,
+  setInfo,
+}: {
+  info: AvailabilityCreator;
+  setInfo: (a: AvailabilityCreator) => void;
+}) {
+  const [showRecurring, setShowRecurring] = useState(false);
+
+  return (
+    <div className="flex-none">
+      <Input
+        className="col-span-2 mt-2"
+        type="date"
+        onChange={(date) => {
+          setInfo({ ...info, date: [date] });
+        }}
+        value={info.date[0]}
+        required
+      />
+
+      <div className="grid grid-cols-2 gap-2 mt-2">
+        <div>
+          <label className="font-body font-bold text-xl" htmlFor="start">
+            Start
+          </label>
+          <Input
+            id="start"
+            type="time"
+            onChange={(start) => {
+              setInfo({ ...info, start });
+            }}
+            value={info.start}
+            required
+          />
+        </div>
+        <div>
+          <div className="flex justify-end">
+            <label className="font-body font-bold text-xl" htmlFor="end">
+              End
+            </label>
+          </div>
+          <Input
+            value={info.end}
+            id="end"
+            type="time"
+            onChange={(end) => {
+              setInfo({ ...info, end });
+            }}
+            required
+          />
+        </div>
+      </div>
+
+      <div className="flex mt-2">
+        <RadioButton
+          label="One time"
+          value="once"
+          checked={!showRecurring}
+          onChange={(v) => {
+            setShowRecurring(!v);
+          }}
+          isFirst
+        />
+        <RadioButton
+          label="Reoccurring"
+          value="reoccurring"
+          checked={showRecurring}
+          isLast
+          onChange={(v) => {
+            setShowRecurring(v);
+          }}
+        />
+      </div>
+      {showRecurring && (
+        <div className="flex gap-2 mt-2">
+          <div className="relative bg-p-grey-100 text-white rounded-lg p-2 flex-none w-16">
+            <input
+              className="font-body text-2xl font-bold text-white w-full bg-transparent focus:outline-none inline-flex items-center w-full transform transition duration-150 ease-in-out m-0"
+              type="number"
+              min={1}
+              step={1}
+              onChange={(e) => {
+                setInfo({ ...info, recurring: Number(e.target.value) });
+              }}
+              placeholder="0"
+              value={info.recurring ? String(info.recurring) : ""}
+            />
+          </div>
+          <select
+            className="flex-auto text-white font-body text-2xl font-bold bg-p-grey-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg px-5 py-2.5 text-center inline-flex items-center w-full"
+            onChange={(e) => {
+              setInfo({ ...info, recurringUnit: e.target.value });
+            }}
+            value={info.recurringUnit ?? ""}
+            required
+          >
+            <option disabled value="">
+              Select interval
+            </option>
+            <option value="days">Days</option>
+            <option value="weeks">Weeks</option>
+            <option value="months">Months</option>
+            <option value="years">Years</option>
+          </select>
+        </div>
+      )}
+      <hr className="w-full h-1 bg-black rounded-full mt-2" />
+    </div>
+  );
+}
 
 export function SetAvailabilityBody({
   info,
@@ -32,7 +228,7 @@ export function SetAvailabilityBody({
   setRefresh: (f: () => void) => void;
 }) {
   const api = useContext(API);
-  const [showRecurring, setShowRecurring] = useState(false);
+
   const [availability, setAvailability] = useState<Availability[]>([]);
   const [recurringAvailability, setRecurringAvailabilities] = useState<
     RecurringAvailability[]
@@ -68,105 +264,11 @@ export function SetAvailabilityBody({
 
   return (
     <>
-      <div className="flex-none">
-        <Input
-          className="col-span-2 mt-2"
-          type="date"
-          onChange={(date) => {
-            setInfo({ ...info, date });
-          }}
-          value={info.date}
-          required
-        />
-
-        <div className="grid grid-cols-2 gap-2 mt-2">
-          <div>
-            <label className="font-body font-bold text-xl" htmlFor="start">
-              Start
-            </label>
-            <Input
-              id="start"
-              type="time"
-              onChange={(start) => {
-                setInfo({ ...info, start });
-              }}
-              value={info.start}
-              required
-            />
-          </div>
-          <div>
-            <div className="flex justify-end">
-              <label className="font-body font-bold text-xl" htmlFor="end">
-                End
-              </label>
-            </div>
-            <Input
-              value={info.end}
-              id="end"
-              type="time"
-              onChange={(end) => {
-                setInfo({ ...info, end });
-              }}
-              required
-            />
-          </div>
-        </div>
-
-        <div className="flex mt-2">
-          <RadioButton
-            label="One time"
-            value="once"
-            checked={!showRecurring}
-            onChange={(v) => {
-              setShowRecurring(!v);
-            }}
-            isFirst
-          />
-          <RadioButton
-            label="Reoccurring"
-            value="reoccurring"
-            checked={showRecurring}
-            isLast
-            onChange={(v) => {
-              setShowRecurring(v);
-            }}
-          />
-        </div>
-        {showRecurring && (
-          <div className="flex gap-2 mt-2">
-            <div className="relative bg-p-grey-100 text-white rounded-lg p-2 flex-none w-16">
-              <input
-                className="font-body text-2xl font-bold text-white w-full bg-transparent focus:outline-none inline-flex items-center w-full transform transition duration-150 ease-in-out m-0"
-                type="number"
-                min={1}
-                step={1}
-                onChange={(e) => {
-                  setInfo({ ...info, recurring: Number(e.target.value) });
-                }}
-                placeholder="0"
-                value={info.recurring ? String(info.recurring) : ""}
-              />
-            </div>
-            <select
-              className="flex-auto text-white font-body text-2xl font-bold bg-p-grey-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg px-5 py-2.5 text-center inline-flex items-center w-full"
-              onChange={(e) => {
-                setInfo({ ...info, recurringUnit: e.target.value });
-              }}
-              value={info.recurringUnit ?? ""}
-              required
-            >
-              <option disabled value="">
-                Select interval
-              </option>
-              <option value="days">Days</option>
-              <option value="weeks">Weeks</option>
-              <option value="months">Months</option>
-              <option value="years">Years</option>
-            </select>
-          </div>
-        )}
-        <hr className="w-full h-1 bg-black rounded-full mt-2" />
-      </div>
+      {__SHOW_ALT_AVAIL__ ? (
+        <NewForm info={info} setInfo={setInfo} />
+      ) : (
+        <OldForm info={info} setInfo={setInfo} />
+      )}
 
       <div className="flex-1 h-full py-2">
         <Feed
@@ -240,38 +342,49 @@ export function SetAvailability() {
   const api = useContext(API);
 
   const [info, setInfo] = useState<AvailabilityCreator>({
-    date: "",
+    date: __SHOW_ALT_AVAIL__ ? [] : [""],
     start: "",
     end: "",
     recurring: undefined,
     recurringUnit: undefined,
   });
+
   const [refresh, setRefresh] = useState<(() => void) | undefined>();
 
   const onSubmit = useCallback(async () => {
-    const { date, start, end, recurring, recurringUnit } = info;
+    await Promise.all(
+      info.date.map(async (date) => {
+        const { start, end, recurring, recurringUnit } = info;
 
-    if (!date || !start || !end)
-      throw Error("Required fields were not filled in");
+        if (!date || !start || !end)
+          throw Error("Required fields were not filled in");
 
-    const req: Availability = {
-      timeStart: moment(`${date} ${start}`).toISOString(),
-      timeEnd: moment(`${date} ${end}`).toISOString(),
-    };
+        const req: Availability = {
+          timeStart: moment(
+            `${moment(date).format("YYYY-MM-DD")} ${start}`,
+          ).toISOString(),
+          timeEnd: moment(
+            `${moment(date).format("YYYY-MM-DD")} ${end}`,
+          ).toISOString(),
+        };
 
-    if (recurring) {
-      if (!recurring || !recurringUnit)
-        throw Error("Required fields were not filled in");
-      req.recurring = {};
-      req.recurring[recurringUnit as "days" | "weeks" | "months" | "years"] =
-        recurring;
-    }
+        if (recurring) {
+          if (!recurring || !recurringUnit)
+            throw Error("Required fields were not filled in");
+          req.recurring = {};
+          req.recurring[
+            recurringUnit as "days" | "weeks" | "months" | "years"
+          ] = recurring;
+        }
 
-    await api.user().addAvailability(req);
+        await api.user().addAvailability(req);
+      }),
+    );
+
     if (refresh) refresh();
 
     setInfo({
-      date: "",
+      date: __SHOW_ALT_AVAIL__ ? [] : [""],
       start: "",
       end: "",
       recurring: undefined,
